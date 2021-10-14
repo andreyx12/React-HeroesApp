c65e3672f58bd3763c96c0c41b1b59501351182b
Andrey <andreyx12345@gmail.com>
2021-10-14 15:17:37 -0600

    test22

---
+++ docs/precache-manifest.c7218e21c422715b7684c371ab1e2721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cc2b33d0fe2d166b12b5e7b0f9b3e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "decebd6365b061130f6c",
+    "url": "/static/js/2.52c27324.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52c27324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ebbbb95b3d7c4aae38",
+    "url": "/static/js/main.2c1945e3.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
